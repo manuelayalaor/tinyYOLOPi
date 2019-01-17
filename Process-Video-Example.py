7786892c50a8da596888e300112b26e25dc9fa98
kenneth-rosario <kenneth.rosario7@upr.edu>
2019-01-17 11:35:53 -0400

    Video Processing Fixed

--- Process-Video-Example.py
+++ Process-Video-Example.py
@@ -3,7 +3,6 @@ import os
 import cv2
 
 
-global CV_CAP_PROP_POS_FRAMES = 1
 
 
 def video_path(file=""):
@@ -26,18 +25,18 @@ def sift_through_vid(position=None, cap=None):
         if flag:
             # The frame is ready and already captured
             cv2.imshow('video', frame)
-            pos_frame = cap.get(CV_CAP_PROP_POS_FRAMES)
+            pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
             print(str(pos_frame)+" frames")
         else:
             # The next frame is not ready, so we try to read it again
-            cap.set(CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+            cap.set(cv2.CAP_PROP_POS_FRAMES, pos_frame-1)
             print('frame is not ready')
             # It is better to wait for a while for the next frame to be ready
             cv2.waitKey(1000)
 
         if cv2.waitKey(10) == 27:
             break
-        if cap.get(CV_CAP_PROP_POS_FRAMES) == cap.get(cv2.CV_CAP_PROP_FRAME_COUNT):
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
             # If the number of captured frames is equal to the total number of frames,
             # we stop
             break
@@ -56,11 +55,12 @@ def manage_video(vid=""):
 def  process_video(vid=""):
     try:
         cap = manage_video(vid)
-        pos_frame = cap.get(CV_CAP_PROP_POS_FRAMES)
+        pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
         sift_through_vid(position=pos_frame, cap=cap) #goes through the video from the position frame
     except Exception  as e:
         print(e)
     finally:
         print('Session Terminated...')
-
     return
+
+process_video("pedestrian-dataset/crosswalk.avi")
