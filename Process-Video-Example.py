eb9ec7a8e9991d2e3bb267f4c1c93bb1035371ce
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2019-01-17 08:38:37 -0400

    Cleaned up video processing, made more modular and added error handling.

--- Process-Video-Example.py
+++ Process-Video-Example.py
@@ -1,29 +1,64 @@
+import os
+
 import cv2
 
-cap = cv2.VideoCapture("./out.mp4")
-while not cap.isOpened():
-    cap = cv2.VideoCapture("./out.mp4")
-    cv2.waitKey(1000)
-    print "Wait for the header"
 
-pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-while True:
-    flag, frame = cap.read()
-    if flag:
-        # The frame is ready and already captured
-        cv2.imshow('video', frame)
-        pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-        print str(pos_frame)+" frames"
+
+def video_path(file=""):
+    file = file.strip()
+    if file is "":
+        raise ValueError('No filename provided!')
+    if os.path.exists(os.path.abspath(file)):
+        return os.path.abspath(file)
     else:
-        # The next frame is not ready, so we try to read it again
-        cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
-        print "frame is not ready"
-        # It is better to wait for a while for the next frame to be ready
-        cv2.waitKey(1000)
+        raise OSError('No Such File Found!')
 
-    if cv2.waitKey(10) == 27:
-        break
-    if cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) == cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
-        # If the number of captured frames is equal to the total number of frames,
-        # we stop
-        break
+
+def sift_through_vid(position=None, cap=None):
+    if cap is None:
+        raise ValueError('No VideoCapture Object was provided')
+    if position is None:
+        raise ValueError('No position frame was provided')
+    while True:
+        flag, frame = cap.read()
+        if flag:
+            # The frame is ready and already captured
+            cv2.imshow('video', frame)
+            pos_frame = cap.get(cv2.CV_CAP_PROP_POS_FRAMES)
+            print(str(pos_frame)+" frames")
+        else:
+            # The next frame is not ready, so we try to read it again
+            cap.set(cv2.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+            print('frame is not ready')
+            # It is better to wait for a while for the next frame to be ready
+            cv2.waitKey(1000)
+
+        if cv2.waitKey(10) == 27:
+            break
+        if cap.get(cv2.CV_CAP_PROP_POS_FRAMES) == cap.get(cv2.CV_CAP_PROP_FRAME_COUNT):
+            # If the number of captured frames is equal to the total number of frames,
+            # we stop
+            break
+
+
+def manage_video(vid=""):
+    vid = video_path(vid)
+    cap = cv2.VideoCapture(vid)
+    while not cap.isOpened():
+        cap = cv2.VideoCapture(vid)
+        cv2.waitKey(1000)
+        print('Waiting for video Header')
+    return cap
+
+
+def  process_video(vid=""):
+    try:
+        cap = manage_video(vid)
+        pos_frame = cap.get(cv2.CV_CAP_PROP_POS_FRAMES)
+        sift_through_vid(position=pos_frame, cap=cap) #goes through the video from the position frame
+    except Exception  as e:
+        print(e)
+    finally:
+        print('Session Terminated...')
+
+    return
