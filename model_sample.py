68bb6426cafdc5a392183065848af7720282d452
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2019-02-04 18:17:45 -0400

    IDK what i did here but ok

--- model_sample.py
+++ model_sample.py
@@ -4,17 +4,23 @@ from tf.keras.models import Sequential
 from tf.keras.layers import  Dense, GlobalAveragePooling1D, MaxPool1D, Embedding, Dropout, Conv1D
 from tf.python.keras.layers import MaxPool2D
 
-global classes = 10
-def create_model():
+classes = 2 #HUMAN or not HUMAN
+RANK = 3
+DIM = 3
+
+def create_model(input_shape=(1920,1080,1)):
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(units=32, kernel_size=(DIM,RANK),
+                     activation='relu', input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
+    model.add(Dense(units=128, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(classes, activation='softmax'))
+    model.add(Dense(units=classes, activation='softmax'))
+
+    #configures learning process
     model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.Adadelta(),
+                  optimizer='rmsprop' ,#keras.optimizers.Adadelta(),
                    metrics=['accuracy'])
     return model
