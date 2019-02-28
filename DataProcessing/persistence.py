38b62efd88a4c2c59b55c837c5f1d8a616c64e47
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2019-02-27 20:23:41 -0400

    Added some scripts for data wrangling(saving, loading and plotting), modded dumb_model w\ a loss func

---
+++ DataProcessing/persistence.py
@@ -0,0 +1,40 @@
+import csv
+import os
+
+def retrieve_file(filename=""):
+
+    if os.path.isfile(os.path.abspath(filename)):
+        return os.path.abspath(filename)
+    else:
+        raise IOError(f" File {filename}: was not found")
+
+#TODO make this a generator instead of loading all data directly
+#This causes memory to fill up rapidly
+def retrieve_data(name):
+    '''Returns a list of dicts representing the csv filename provided'''
+    #Assumes csv extension is already provided
+    data=[]
+    if(os.path.exists(name)):
+        try:
+            with open(name,'rt') as fin:
+                cin = csv.DictReader(fin)
+                data = [row for row in cin]
+        except IOError as detail:
+            print('Run-time error while reading data:', detail)
+
+    else:
+        raise IOError(f'File with name {name} does not exist!')
+
+    return data
+
+def write_data(data={}, filename=""):
+    try:
+        with open(name,'wt') as fout:
+            cout = csv.writer(fout,quoting=csv.QUOTE_NONE)#Likely candidate since it usually processes a list of lists
+            #Confirmed must take a list of list, Tups or Dicts
+            #Works fine now
+            cout.writerows(data)
+    except IOError as e:
+        print("[#]Error while writing CSV data:", e)
+        return False
+    return True#success
--- model/dumb_model.py
+++ model/dumb_model.py
@@ -34,6 +34,19 @@ def prep_data():
     shapes = np.zeros((num_imgs, num_objects), dtype=int)
     return bboxes, imgs, shapes
 
+def loss_func_params(_class_pred=[], class_labels=[]):#based on YOLO paper
+    params = {}
+    lam_coord = 5
+    lam_no_obj = .5
+    prob_class = 0.0
+    pred_prob_class = 0.0
+    return params
+
+def loss_func(params={}):
+
+
+    return ans
+
 def train_model():
     bboxes, imgs, shapes = prep_data()
     num_imgs = 50000
@@ -45,58 +58,58 @@ def train_model():
     num_shapes = 3
     shape_labels = ['rectangle', 'circle', 'triangle']
     for i_img in range(num_imgs):
-    surface = cairo.ImageSurface.create_for_data(imgs[i_img], cairo.FORMAT_ARGB32, img_size, img_size)
-    cr = cairo.Context(surface)
+        surface = cairo.ImageSurface.create_for_data(imgs[i_img], cairo.FORMAT_ARGB32, img_size, img_size)
+        cr = cairo.Context(surface)
 
-    # Fill background white.
-    cr.set_source_rgb(1, 1, 1)
-    cr.paint()
+        # Fill background white.
+        cr.set_source_rgb(1, 1, 1)
+        cr.paint()
 
-    # TODO: Try no overlap here.
-    # Draw random shapes.
-    for i_object in range(num_objects):
-        shape = np.random.randint(num_shapes)
-        shapes[i_img, i_object] = shape
-        if shape == 0:  # rectangle
-            w, h = np.random.randint(min_object_size, max_object_size, size=2)
-            x = np.random.randint(0, img_size - w)
-            y = np.random.randint(0, img_size - h)
-            bboxes[i_img, i_object] = [x, y, w, h]
-            cr.rectangle(x, y, w, h)
-        elif shape == 1:  # circle
-            r = 0.5 * np.random.randint(min_object_size, max_object_size)
-            x = np.random.randint(r, img_size - r)
-            y = np.random.randint(r, img_size - r)
-            bboxes[i_img, i_object] = [x - r, y - r, 2 * r, 2 * r]
-            cr.arc(x, y, r, 0, 2*np.pi)
-        elif shape == 2:  # triangle
-            w, h = np.random.randint(min_object_size, max_object_size, size=2)
-            x = np.random.randint(0, img_size - w)
-            y = np.random.randint(0, img_size - h)
-            bboxes[i_img, i_object] = [x, y, w, h]
-            cr.move_to(x, y)
-            cr.line_to(x+w, y)
-            cr.line_to(x+w, y+h)
-            cr.line_to(x, y)
-            cr.close_path()
+        # TODO: Try no overlap here.
+        # Draw random shapes.
+        for i_object in range(num_objects):
+            shape = np.random.randint(num_shapes)
+            shapes[i_img, i_object] = shape
+            if shape == 0:  # rectangle
+                w, h = np.random.randint(min_object_size, max_object_size, size=2)
+                x = np.random.randint(0, img_size - w)
+                y = np.random.randint(0, img_size - h)
+                bboxes[i_img, i_object] = [x, y, w, h]
+                cr.rectangle(x, y, w, h)
+            elif shape == 1:  # circle
+                r = 0.5 * np.random.randint(min_object_size, max_object_size)
+                x = np.random.randint(r, img_size - r)
+                y = np.random.randint(r, img_size - r)
+                bboxes[i_img, i_object] = [x - r, y - r, 2 * r, 2 * r]
+                cr.arc(x, y, r, 0, 2*np.pi)
+            elif shape == 2:  # triangle
+                w, h = np.random.randint(min_object_size, max_object_size, size=2)
+                x = np.random.randint(0, img_size - w)
+                y = np.random.randint(0, img_size - h)
+                bboxes[i_img, i_object] = [x, y, w, h]
+                cr.move_to(x, y)
+                cr.line_to(x+w, y)
+                cr.line_to(x+w, y+h)
+                cr.line_to(x, y)
+                cr.close_path()
 
-        # TODO: Introduce some variation to the colors by adding a small random offset to the rgb values.
-        color = np.random.randint(num_colors)
-        colors[i_img, i_object] = color
-        max_offset = 0.3
-        r_offset, g_offset, b_offset = max_offset * 2. * (np.random.rand(3) - 0.5)
-        if color == 0:
-            cr.set_source_rgb(1-max_offset+r_offset, 0+g_offset, 0+b_offset)
-        elif color == 1:
-            cr.set_source_rgb(0+r_offset, 1-max_offset+g_offset, 0+b_offset)
-        elif color == 2:
-            cr.set_source_rgb(0+r_offset, 0-max_offset+g_offset, 1+b_offset)
-        cr.fill()
+            # TODO: Introduce some variation to the colors by adding a small random offset to the rgb values.
+            color = np.random.randint(num_colors)
+            colors[i_img, i_object] = color
+            max_offset = 0.3
+            r_offset, g_offset, b_offset = max_offset * 2. * (np.random.rand(3) - 0.5)
+            if color == 0:
+                cr.set_source_rgb(1-max_offset+r_offset, 0+g_offset, 0+b_offset)
+            elif color == 1:
+                cr.set_source_rgb(0+r_offset, 1-max_offset+g_offset, 0+b_offset)
+            elif color == 2:
+                cr.set_source_rgb(0+r_offset, 0-max_offset+g_offset, 1+b_offset)
+            cr.fill()
 
         imgs = imgs[..., 2::-1]  # is BGRA, convert to RGB
 
         # surface.write_to_png('imgs/{}.png'.format(i_img))
-        imgs.shape, bboxes.shape, shapes.shape, colors.shape
+        return imgs.shape, bboxes.shape, shapes.shape, colors.shape
 
 if __name__ == "__main__":
     simple_model()
