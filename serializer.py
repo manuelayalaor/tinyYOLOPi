20debbd4f58ff9cc850ec50504aba7fcb17c48ec
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2019-03-28 14:45:16 -0400

    added some code samples from keras docs

--- serializer.py
+++ serializer.py
@@ -1,14 +1,28 @@
 import jsonify
 
 class Serializer():
+
     def __init__(self, model):
         '''Receives model'''
+        self.model = model
+        return
+    #Context Manager Logic
+    def __enter__():
         pass
-
+    def __exit__():
+        pass
+    ###
+    
     def extract(self):
         '''Extract info from model'''
-        pass
-    def dump(self):
+        #This is problematic
+        layer = self.model.get_layer(name=None, index=None)
+        #assumes we already now the depth of the Network
+
+        config = layer.get_config()#this returns a dict
+        return
+
+    def dump(self,filename=""):
         #do json stuff here
-        '''dump the info into a file as a json'''
-        pass
+        '''Dump the extracted info into a file as a json'''
+        return
